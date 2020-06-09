ab3e6e934bb995256b68a5ecb90b79226c586d9c
Abbas Mokhtari <abs.mokhtari@gmail.com>
2020-06-09 21:26:35 +0100

    before language

--- main.js
+++ main.js
@@ -7,28 +7,62 @@ fetch('./data.json')
     let testelement = data.map((x) => x.position);
     console.log(testelement);
     data
-      .map((x) => mainBlock(x.logo, x.company, x.position, x.postedAt, x.contract, x.location, x.new, x.featured))
+      .map((x) =>
+        mainBlock(
+          x.logo,
+          x.company,
+          x.position,
+          x.postedAt,
+          x.contract,
+          x.location,
+          x.new,
+          x.featured,
+          x.role,
+          x.level,
+          x.language
+        )
+      )
       .forEach((x) => rootElement.appendChild(x));
   });
 
-const mainBlock = (logo, company, position, postedAt, contract, location, addedNew, addFeatured) => {
+const mainBlock = (
+  logo,
+  company,
+  position,
+  postedAt,
+  contract,
+  location,
+  addedNew,
+  addFeatured,
+  role,
+  level,
+  language
+) => {
   let mainDiv = document.createElement('div');
   let logoElement = document.createElement('img');
   let companyName = document.createElement('p');
   let positionName = document.createElement('p');
   let jobStatus = document.createElement('p');
-  let newItem = document.createElement('div')
-  let featuredItem = document.createElement('div')
-  let requiredSkills = document.createElement('div')
+  let newItem = document.createElement('div');
+  let featuredItem = document.createElement('div');
+  let requiredSkills = document.createElement('div');
+  let roleItem = document.createElement('div');
+  let levelItem = document.createElement('div');
+  let languageItem = document.createElement('div');
 
   logoElement.src = logo;
   companyName.innerText = company;
   positionName.innerText = position;
   jobStatus.innerText = `${postedAt} - ${contract} - ${location}`;
-  addedNew ? newItem.innerText = 'NEW': newItem.innerText = '';
-  addFeatured ? featuredItem.innerText = 'FEATURED': featuredItem.innerText = '';
+  addedNew ? (newItem.innerText = 'NEW') : (newItem.innerText = '');
+  addFeatured
+    ? (featuredItem.innerText = 'FEATURED')
+    : (featuredItem.innerText = '');
+  roleItem.innerText = role;
+  levelItem.innerText = level;
 
-
+  requiredSkills.appendChild(roleItem);
+  requiredSkills.appendChild(levelItem);
 
   mainDiv.appendChild(logoElement);
   mainDiv.appendChild(companyName);
@@ -36,6 +70,7 @@ const mainBlock = (logo, company, position, postedAt, contract, location, addedN
   mainDiv.appendChild(jobStatus);
   mainDiv.appendChild(newItem);
   mainDiv.appendChild(featuredItem);
+  mainDiv.appendChild(requiredSkills);
 
   return mainDiv;
 };
